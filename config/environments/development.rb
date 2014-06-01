b4105314536d8b40c28da049c950d492958acc1f
Daniel Cornnell <daniel.cornnell@gmail.com>
2014-05-31 22:56:10 -0400

    is this fixed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,8 +34,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.sendmail_settings = {
     address: 'smtp.gmail.com',
     port: 587,
     domain: 'gmail.com',
@@ -45,6 +45,4 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_options = {from: 'dcornnel@gmail.com'}
-
-
 end
